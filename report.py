8e7f2ac7a7746504fed87e868bd3bdc8d4fddfca
Ben Wallberg <wallberg@umd.edu>
2023-10-16 14:08:53 -0400

    Skip Users and Forms marked Keep

--- report.py
+++ report.py
@@ -34,6 +34,15 @@ urllib.request.install_opener(opener)
 def get_users():
     ''' Get the list of users. '''
 
+    # Get list of users to skip
+    users_skip = set()
+    with open('Users-Done.csv', 'r') as users_done_file:
+        reader = csv.DictReader(users_done_file)
+        for row in reader:
+            if row['Action'] == 'Keep':
+                users_skip.add((row['User'], row['Email']))
+
+    # Query Wufoo API for list of users
     response = urllib.request.urlopen(env['base_url']+'users.json')
     data = json.load(response)
 
@@ -45,6 +54,13 @@ def get_users():
             writer.writerow(USER_COLS)
 
             for user in users:
+
+                # Determine if this user should be skipped
+                if (user['User'], user['Email']) in users_skip:
+                    print('Skipping User', user)
+                    continue
+
+                # Write the user
                 row = []
                 for col in USER_COLS:
                     row.append(user[col])
@@ -96,6 +112,19 @@ def add_form_entries(form):
 def get_forms():
     ''' Get the list of forms. '''
 
+    # Get list of forms to skip
+    forms_skip = set()
+    with open('Forms-Done.csv', 'r') as forms_done_file:
+        reader = csv.DictReader(forms_done_file)
+        for row in reader:
+            if row['Action'] == 'Keep':
+                key = (row['Name'], row['DateCreated'])
+                if key in forms_skip:
+                    print(f'forms_skip key collision: {key}')
+                forms_skip.add(key)
+    print(forms_skip)
+
+    # Query Wufoo API for list of forms
     response = urllib.request.urlopen(env['base_url']+'forms.json')
     data = json.load(response)
 
@@ -107,9 +136,19 @@ def get_forms():
             writer.writerow(FORM_COLS)
 
             for form in forms:
-                row = []
 
+                # Determine if this form should be skipped
+                key = (form['Name'], form['DateCreated'][0:10])
+                print('Checking key', key)
+                if key in forms_skip:
+                    print('Skipping Form', form)
+                    continue
+
+                # Add form entry information
                 add_form_entries(form)
+
+                # Write the form
+                row = []
 
                 for col in FORM_COLS:
                     row.append(form[col])
